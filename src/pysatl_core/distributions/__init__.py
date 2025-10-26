dca9d3ed5829fdab44484680c0c24dc2fb7aca02
LeonidElkin <113133848+LeonidElkin@users.noreply.github.com>
2025-10-26 18:55:48 +0300

    refactor: delete GenericCharacteristic

--- src/pysatl_core/distributions/__init__.py
+++ src/pysatl_core/distributions/__init__.py
@@ -58,8 +58,6 @@ __author__ = "Leonid Elkin, Mikhail, Mikhailov"
 __copyright__ = "Copyright (c) 2025 PySATL project"
 __license__ = "SPDX-License-Identifier: MIT"
 
-# Re-exports: characteristics
-from .characteristics import GenericCharacteristic
 
 # Re-exports: computation primitives and canonical conversions
 from .computation import (
