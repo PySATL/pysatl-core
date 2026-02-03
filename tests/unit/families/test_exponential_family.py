8f69580ab156036b66cd5822a6bfad456bbc326d
domosedy <ilayvin05@mail.ru>
2026-02-03 14:46:50 +0300

    [refactor] refactoring for mypy

--- tests/unit/families/test_exponential_family.py
+++ tests/unit/families/test_exponential_family.py
@@ -16,14 +16,14 @@ from pysatl_core.types import UnivariateContinuous
 
 
 def gamma_pdf(alpha: float, beta: float, x: float) -> float:
-    return scipy.stats.gamma(a=alpha, scale=1 / beta).pdf(x).item()
+    return scipy.stats.gamma(a=alpha, scale=1 / beta).pdf(x).item()  # type: ignore[attr-defined]
 
 
 @pytest.fixture(scope="function")
 def conjugate_for_exponential() -> ExponentialFamily:
     def get_parameter_from_natural_parameter(
-        eta_parametrization: ExponentialFamilyParametrization,
-    ):
+        eta_parametrization: Any,
+    ) -> Any:
         if hasattr(eta_parametrization, "__len__"):
             if len(eta_parametrization) > 1:
                 return list(-1 * np.array(eta_parametrization))
@@ -34,18 +34,15 @@ def conjugate_for_exponential() -> ExponentialFamily:
         theta_parametrization: Any,
     ) -> Any:
         if type(theta_parametrization) is ExponentialFamilyParametrization:
-            theta_parametrization = cast(
-                ExponentialFamilyParametrization, theta_parametrization
-            )
-            eta = -theta_parametrization.theta
+            eta = list(-np.array(theta_parametrization.theta))
             return ExponentialFamilyParametrization(theta=eta)
 
         return -1 * theta_parametrization
 
-    def transform_function(x: list[Any]) -> list[Any]:
-        if type(x) is not list:
-            return -x
-        return [-x[0]]
+    def transform_function(x: list[float] | float) -> list[float] | float:
+        if type(x) is list:
+            return [-x[0]]
+        return -x  # type: ignore[operator]
 
     fam = ExponentialFamily(
         log_partition=lambda parametrization: np.log(parametrization.theta[0]),
@@ -77,16 +74,12 @@ def test_exponential_pdf(theta1, theta2, conjugate_for_exponential):
     alpha = theta2 + 1
     beta = theta1
 
-    exponential = gamma_family(
-        theta=np.array([theta1, theta2]), parametrization_name="theta"
-    )
+    exponential = gamma_family(theta=np.array([theta1, theta2]), parametrization_name="theta")
     pdf = exponential.computation_strategy.query_method("pdf", distr=exponential)
 
     x = [i / 10 for i in range(100)]
 
-    assert_allclose(
-        [pdf(xx) for xx in x], [gamma_pdf(alpha, beta, xx) for xx in x], rtol=1e-6
-    )
+    assert_allclose([pdf(xx) for xx in x], [gamma_pdf(alpha, beta, xx) for xx in x], rtol=1e-6)
 
 
 @pytest.mark.parametrize("theta1", range(2, 5))
@@ -97,9 +90,7 @@ def test_exponential_mean(theta1, theta2, conjugate_for_exponential):
     alpha = theta2 + 1
     beta = theta1
 
-    exponential = gamma_family(
-        theta=np.array([theta1, theta2]), parametrization_name="theta"
-    )
+    exponential = gamma_family(theta=np.array([theta1, theta2]), parametrization_name="theta")
     mean = exponential.computation_strategy.query_method("mean", distr=exponential)
     assert np.isclose(mean(12), alpha / beta, rtol=1e-6)
 
@@ -112,8 +103,6 @@ def test_exponential_var(theta1, theta2, conjugate_for_exponential):
     alpha = theta2 + 1
     beta = theta1
 
-    exponential = gamma_family(
-        theta=np.array([theta1, theta2]), parametrization_name="theta"
-    )
+    exponential = gamma_family(theta=np.array([theta1, theta2]), parametrization_name="theta")
     var = exponential.computation_strategy.query_method("var", distr=exponential)
     assert np.isclose(var(12), alpha / beta**2, rtol=1e-6)
