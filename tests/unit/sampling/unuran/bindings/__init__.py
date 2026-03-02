7abe0dd0f01ee8ade5b6f4c41605d47a491660e3
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2026-03-02 20:42:54 +0300

    feat: UNURAN as submodule with build

---
+++ tests/unit/sampling/unuran/bindings/__init__.py
@@ -0,0 +1,11 @@
+"""
+UNU.RAN Bindings Tests
+======================
+
+Unit tests for the UNU.RAN CFFI bindings layer, covering the build
+configuration and the compiled extension availability.
+"""
+
+__author__ = "Artem Romanyuk"
+__copyright__ = "Copyright (c) 2025 PySATL project"
+__license__ = "SPDX-License-Identifier: MIT"
